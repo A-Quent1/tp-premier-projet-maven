693d04c376cc075707a96ad7f4df3e62b52e8866
A-Quent1 <annezo.quentin@gmail.com>
2022-10-24 16:04:56 +0200

    Commit fin tp3 ( profil )

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,5 +1,6 @@
 package dev;
 
+import java.util.ResourceBundle;
 import com.github.lalyos.jfiglet.FigletFont;
 
 /**
@@ -10,8 +11,13 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        String asciiArt = FigletFont.convertOneLine("App");
+    	System.out.println( "Hello World!" );
+    	
+    	String titre = ResourceBundle.getBundle("application").getString("titre");
+        String asciiArt = FigletFont.convertOneLine(titre);
         System.out.println(asciiArt);
+    	
+        String environnement = ResourceBundle.getBundle("application").getString("environnement");
+        System.out.println("Environnement : "+environnement);
     }
 }
